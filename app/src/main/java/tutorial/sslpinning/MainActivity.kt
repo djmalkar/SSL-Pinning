a7f13aeaed3ca5f12f0d76e1f1d2e8496fefb249
Dipesh <djmalkar840@gmail.com>
2024-08-10 13:12:41 +0530

    Toolbar added

--- app/src/main/java/tutorial/sslpinning/MainActivity.kt
+++ app/src/main/java/tutorial/sslpinning/MainActivity.kt
@@ -12,9 +12,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,95 +34,113 @@ import javax.net.ssl.SSLHandshakeException
 private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SSLPinningTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Spacer(
-                            modifier = Modifier
-                                .height(20.dp)
-                        )
-
-                        /**
-                         * Showcase how website if pinned to proper certificate leads to successful
-                         * Handshake
-                         */
-                        FilledTonalButton(
-                            onClick = { testUrlConnection("https://www.wikipedia.org/") },
-                            colors = ButtonDefaults.buttonColors(
-                                containerColor = Color.Green,
-                                contentColor = Color.DarkGray
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text("SSL Pinning")
+                            },
+                            colors = TopAppBarColors(
+                                containerColor = Color.LightGray,
+                                scrolledContainerColor = Color.White,
+                                navigationIconContentColor = Color.White,
+                                titleContentColor = Color.Black,
+                                actionIconContentColor = Color.Black
                             )
-                        ) {
-                            Text("SSL Certificate Connection Pass")
-                        }
-
-                        Spacer(
-                            modifier = Modifier
-                                .height(20.dp)
                         )
-
-                        /**
-                         * Showcase how website if pinned to wrong certificate leads to Handshake
-                         * Fail
-                         */
-                        FilledTonalButton(
-                            onClick = { testUrlConnection("https://www.google.co.in/") },
-                            colors = ButtonDefaults.buttonColors(
-                                containerColor = Color.Red,
-                                contentColor = Color.White
-                            )
-                        ) {
-                            Text("SSL Certificate Connection Fail")
-                        }
-
-                        Spacer(
+                   },
+                    content = { innerPadding ->
+                        Column(
                             modifier = Modifier
-                                .height(20.dp)
-                        )
-
-                        /**
-                         * Showcase how website if pinned to correct SHA256 hash of public key
-                         * leads to successful Handshake
-                         */
-                        FilledTonalButton(
-                            onClick = { testUrlConnection("https://www.facebook.com/") },
-                            colors = ButtonDefaults.buttonColors(
-                                containerColor = Color.Green,
-                                contentColor = Color.DarkGray
-                            )
+                                .padding(innerPadding)
+                                .fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally
                         ) {
-                            Text("SSL Public Key Pinning Pass")
-                        }
-
-                        Spacer(
-                            modifier = Modifier
-                                .height(20.dp)
-                        )
-
-                        /**
-                         * Showcase how website if pinned to correct SHA256 hash of public key
-                         * leads to unsuccessful Handshake
-                         */
-                        FilledTonalButton(
-                            onClick = { testUrlConnection("https://github.com/") },
-                            colors = ButtonDefaults.buttonColors(
-                                containerColor = Color.Red,
-                                contentColor = Color.White
+                            Spacer(
+                                modifier = Modifier
+                                    .height(20.dp)
                             )
-                        ) {
-                            Text("SSL Public Key Pinning Fail")
+
+                            /**
+                             * Showcase how website if pinned to proper certificate leads to successful
+                             * Handshake
+                             */
+                            FilledTonalButton(
+                                onClick = { testUrlConnection("https://www.wikipedia.org/") },
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color.Green,
+                                    contentColor = Color.DarkGray
+                                )
+                            ) {
+                                Text("SSL Certificate Connection Pass")
+                            }
+
+                            Spacer(
+                                modifier = Modifier
+                                    .height(20.dp)
+                            )
+
+                            /**
+                             * Showcase how website if pinned to wrong certificate leads to Handshake
+                             * Fail
+                             */
+                            FilledTonalButton(
+                                onClick = { testUrlConnection("https://www.google.co.in/") },
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color.Red,
+                                    contentColor = Color.White
+                                )
+                            ) {
+                                Text("SSL Certificate Connection Fail")
+                            }
+
+                            Spacer(
+                                modifier = Modifier
+                                    .height(20.dp)
+                            )
+
+                            /**
+                             * Showcase how website if pinned to correct SHA256 hash of public key
+                             * leads to successful Handshake
+                             */
+                            FilledTonalButton(
+                                onClick = { testUrlConnection("https://www.facebook.com/") },
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color.Green,
+                                    contentColor = Color.DarkGray
+                                )
+                            ) {
+                                Text("SSL Public Key Pinning Pass")
+                            }
+
+                            Spacer(
+                                modifier = Modifier
+                                    .height(20.dp)
+                            )
+
+                            /**
+                             * Showcase how website if pinned to correct SHA256 hash of public key
+                             * leads to unsuccessful Handshake
+                             */
+                            FilledTonalButton(
+                                onClick = { testUrlConnection("https://github.com/") },
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color.Red,
+                                    contentColor = Color.White
+                                )
+                            ) {
+                                Text("SSL Public Key Pinning Fail")
+                            }
                         }
                     }
-                }
+                )
             }
         }
     }
